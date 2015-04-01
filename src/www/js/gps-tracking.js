d1ab25144d8087643ad4d1b2481ad8ebb89315f2
George Hamilton <george.hamilton@ed.ac.uk>
2015-04-01 17:35:20 +0100

    Issue edina/fieldtrip-gb#187 createRecord args order wrong order

--- src/www/js/gps-tracking.js
+++ src/www/js/gps-tracking.js
@@ -77,7 +77,9 @@ define(['records', 'map', 'file', 'utils', 'settings', './tracks'], function(// 
                 utils.inform('Required field not populated');
             }
             else{
-                currentGpsAnnotation = records.createRecord(records.EDITOR_GROUP.DEFAULT, 'track.edtr');
+                currentGpsAnnotation = records.createRecord(
+                    'track.edtr',
+                    records.EDITOR_GROUP.DEFAULT);
                 currentGpsAnnotation.record.name = $('#annotate-gps-form-title').val();
                 currentGpsAnnotation.record.properties.fields = [
                             {
